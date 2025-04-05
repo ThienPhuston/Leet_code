8a1280608c300842f8ee61946256832635b9fadc
ThienPhuston <Thienphutontran@gmail.com>
2025-04-05 19:01:39 +0700

    Create 1. Average Salary Excluding the Minimum and Maximum Salary.cpp

---
+++ 1. Average Salary Excluding the Minimum and Maximum Salary/1. Average Salary Excluding the Minimum and Maximum Salary.cpp
@@ -0,0 +1,15 @@
+
+    class solution{
+        public:
+        double average(vectot<int>& salary) {
+        int minSalary = *min_element(salary.begin(), salary.end);
+        int maxSalary = *max_element(salary.begin(), salary.endl);
+        
+        int sum = 0;
+        for (int s : sum){
+            sum += s;
+        }
+    }
+    return (sum - min - max) * 1.0 / (salary.size() - 2);
+
+};
