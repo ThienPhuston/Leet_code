6414a2610d4cf1c321ad69319a0e34d50a67dc04
ThienPhuston <Thienphutontran@gmail.com>
2025-04-05 21:09:50 +0700

    Create 896. Monotonic Array.cpp

---
+++ 896. Monotonic Array/896. Monotonic Array.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+    public:
+        bool isMonotonic(vector<int>& nums) {
+            int n = nums.size();
+            bool Tangdan = true;
+            bool Giamdan = true;
+            for (int i = 1; i < n; i++) {
+                if (nums[i] < nums[i - 1]) {
+                    Giamdan = false;
+                }
+                if (nums[i] > nums[i - 1]) {
+                    Tangdan = false;
+                }
+            }
+            return Tangdan || Giamdan;
+        }
+    };
